abb4eaf393e97ba182a03467c3352d94b6527b81
Heyvlu <3059982570@qq.com>
2020-11-02 19:15:52 +0800

    add file

--- 其他题目/两数之和.js
+++ 其他题目/两数之和.js
@@ -4,12 +4,12 @@
             return false;
         }else{
             for(let i=0;i < nums.length; i++){
-                for(let j=1;j < nums.length-1;j++){
+                for(let j=0;j < nums.length;j++){
                     if( nums[i]+nums[j]===target&&i!==j){
                         return([i,j]);
                     }
                 }
             } 
         } 
-    }
-    console.log(twoSum([],10));
+    };
+    console.log(twoSum([3,3],6));
