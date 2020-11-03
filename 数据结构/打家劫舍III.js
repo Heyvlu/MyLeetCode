54ca1bbdd6fb5054a8645dd830a434b9aa8232ab
Heyvlu <39690975+Heyvlu@users.noreply.github.com>
2020-11-03 21:23:08 +0800

    DP

--- 数据结构/打家劫舍III.js
+++ 数据结构/打家劫舍III.js
@@ -1,41 +1,15 @@
-function TreeNode(val) {
-    this.val = val;
-    this.left = this.right = null;
-}
 var rob = function(root) {
-    if(root == undefined)   //处理特殊情况[]
-        return 0;
-    // 后序遍历, 为每个结点添加max_val和aban.val属性
-    postOrder(root);
-    return root.max_val; 
-};
-
-function postOrder(root)
-{
-    //若左侧不为空结点: 继续遍历
-    if(root.left !== null)
-    {
-        arguments.callee(root.left);
-    }else { //若为空: 给左侧添加一个结点(便于处理)
-        root.left = {
-            aban_val : 0,
-            max_val: 0
-        };
-    }
-
-    // 右侧处理同上
-    if(root.right !== null)
-    {
-        arguments.callee(root.right);
-    }else 
-        root.right = {
-            aban_val : 0,
-            max_val: 0
+    const dfs = (node) => {
+        if (node === null) {
+            return [0, 0];
+        }
+        const l = dfs(node.left);
+        const r = dfs(node.right);
+        const selected = node.val + l[1] + r[1];
+        const notSelected = Math.max(l[0], l[1]) + Math.max(r[0], r[1]);
+        return [selected, notSelected];
     };
-
-    //为该结点添加属性 aban_val和max_val, 其中sel_val为假设当前结点选中的情况下的最大值
-    root.aban_val = root.left.max_val + root.right.max_val;
-    var sel_val = root.left.aban_val + root.right.aban_val + root.val;
-    root.max_val = Math.max(sel_val, root.aban_val);
-}
+    const rootStatus = dfs(root);
+    return Math.max(rootStatus[0], rootStatus[1]);
+};
 console.log(rob([3,2,3,null,3,null,1]));
