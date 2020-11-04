7718baeec5f64cc47b0c60edfc12188dabc8a660
Heyvlu <3059982570@qq.com>
2020-11-04 17:45:22 +0800

    left and right pointer

---
+++ 其他题目/三数之和.js
@@ -0,0 +1,38 @@
+var threeSum = function(nums) {
+    let res=[];
+    const len=nums.length;
+    if(len<3){
+        return res;
+    }
+    nums.sort((a,b)=>a-b);
+    for(let i=0;i<len;i++){
+        if(nums[i]>0){
+            break;
+        }
+        if(i>0 && nums[i]==nums[i-1]){
+            continue;
+        }
+        let L=i+1;
+        let R=len-1;
+        while(L<R){
+            const sum=nums[i]+nums[L]+nums[R];
+            if(sum==0){
+                res.push([nums[i],nums[L],nums[R]]);
+                while(L<R && nums[L]==nums[L+1]){
+                    L++;
+                }
+                while(L<R && nums[R]==nums[R-1]){
+                    R--;
+                }
+                L++;
+                R--;
+            }else if(sum<0){
+                L++;
+            }else{
+                R--;
+            }
+        }
+    }
+    return res;
+};
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
