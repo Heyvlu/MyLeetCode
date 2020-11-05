97e1d879a16e336e1e53c7fe1e15e48fa5d7acc9
Heyvlu <3059982570@qq.com>
2020-11-05 17:56:47 +0800

    recursion

---
+++ 数据结构/二叉树展开为链表.js
@@ -0,0 +1,17 @@
+var flatten = function(root) {
+    if(root===null){
+        return;
+    }
+    flatten(root.left);
+    flatten(root.right);
+    let left=root.left;
+    let right=root.right;
+    root.left=null;
+    root.right=left;
+    let p=root;
+    while(p.right!==null){
+        p=p.right;
+    }
+    p.right=right;
+    return root;
+};
