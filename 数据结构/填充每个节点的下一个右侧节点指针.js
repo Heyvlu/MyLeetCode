0085ad15aa1ae0573b3933c7bddd2c54f808b320
Heyvlu <3059982570@qq.com>
2020-11-05 19:16:05 +0800

    recursion

---
+++ 数据结构/填充每个节点的下一个右侧节点指针.js
@@ -0,0 +1,16 @@
+var connect = function(root) {
+    if(root==null){
+        return null;
+    }
+    let connectTwoNode=(node1,node2)=>{
+        if(node1==null||node2==null){
+            return;
+        }
+        node1.next=node2;
+        connectTwoNode(node1.left,node1.right);
+        connectTwoNode(node2.left,node2.right);
+        connectTwoNode(node1.right,node2.left);
+    };
+    connectTwoNode(root.left,root.right);
+    return root;
+};
