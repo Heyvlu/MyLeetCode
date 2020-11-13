22602d8bb0619ee0f70690b8079b45adfd3ea41e
Heyvlu <3059982570@qq.com>
2020-11-13 20:45:35 +0800

    recursion

---
+++ 动态规划/正则表达式匹配.js
@@ -0,0 +1,38 @@
+var isMatch = function(s1, p1) {
+    let m=s1.length,n=p1.length;
+    let i,j;
+    let dp=(s,i,p,j)=>{
+        if (j == n) {
+            return i == m;
+        }
+        if (i == m) {
+            if ((n - j) % 2 == 1) {
+                return false;
+            }
+            for (; j + 1 < n; j += 2) {
+                if (p[j + 1] != '*') {
+                    return false;
+                }
+            }
+            return true;
+        }
+        let res=false;
+        if (s[i] == p[j] || p[j] == '.') {
+            if (j < n - 1 && p[j + 1] == '*') {
+                res = dp(s, i, p, j + 2)
+                   || dp(s, i + 1, p, j);
+            } else {
+                res = dp(s, i + 1, p, j + 1);
+            }
+        } else {
+            if (j < n - 1 && p[j + 1] == '*') {
+                res = dp(s, i, p, j + 2);
+            } else {
+                res = false;
+            }
+        }
+        return res;
+    };
+    return dp(s1,0,p1,0);
+};
+console.log(isMatch('aa','a*'));
