8c382d6b1db95faefe27aefa3ea8d0f0993b0ccb
Heyvlu <3059982570@qq.com>
2020-11-14 17:41:03 +0800

    two points

---
+++ 算法思维/最长回文子串.js
@@ -0,0 +1,19 @@
+var longestPalindrome = function(s) {
+    let res=new String();
+    let l,r;
+    let palindrome=(s,l,r)=>{
+        while(l>=0 && r<s.length && s[l]==s[r]){
+            l--;
+            r++;
+        }
+        return s.substr(l+1,r-l-1);
+    }
+    for(let i=0;i<s.length;i++){
+        let s1=palindrome(s,i,i);
+        let s2=palindrome(s,i,i+1);
+        res=res.length>s1.length?res:s1;
+        res=res.length>s2.length?res:s2;
+    }
+    return res;
+};
+console.log(longestPalindrome("babad"));
