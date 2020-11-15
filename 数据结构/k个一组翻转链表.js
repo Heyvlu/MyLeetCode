5fed740413a41cd7e425e631c733707ed47f9e80
Heyvlu <3059982570@qq.com>
2020-11-15 10:23:50 +0800

    iteration

---
+++ 数据结构/k个一组翻转链表.js
@@ -0,0 +1,23 @@
+var reverseKGroup = function(head, k) {
+    if(head==null) return null;
+    let a=head,b=head;
+    for(let i=0;i<k;i++){
+        if(b==null) return head;
+        b=b.next;
+    }
+    let reverse=(a,b)=>{
+        let pre=null,
+        cur=a,
+        nxt=a;
+        while(cur!==b){
+            nxt=cur.next;
+            cur.next=pre;
+            pre=cur;
+            cur=nxt;
+        }
+        return pre;
+    };
+    let newHead=reverse(a,b);
+    a.next=reverseKGroup(b,k);
+    return newHead;
+};
