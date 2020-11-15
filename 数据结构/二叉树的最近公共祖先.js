efb1e46026e3f04474bb8d03c003af248d8bc04c
Heyvlu <3059982570@qq.com>
2020-11-15 22:03:40 +0800

    recursion

---
+++ 数据结构/二叉树的最近公共祖先.js
@@ -0,0 +1,13 @@
+var lowestCommonAncestor = function(root, p, q) {
+    if(root==null) return null;
+    if(root==p||root==q) return root;
+    let left=lowestCommonAncestor(root.left,p,q);
+    let right=lowestCommonAncestor(root.right,p,q);
+    if(left!==null && right!==null){
+        return root;
+    }
+    if(left==null && right==null){
+        return null;
+    }
+    return left==null?right:left;
+};
