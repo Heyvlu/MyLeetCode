673eadd381972a86f4526dbd7e627d9ea94dd77f
Heyvlu <3059982570@qq.com>
2020-11-22 19:35:20 +0800

    Fast and slow pointer

--- 数据结构/删除链表的倒数第N个节点.js
+++ 数据结构/删除链表的倒数第N个节点.js
@@ -1,6 +1,6 @@
 var removeNthFromEnd = function(head, n) {
     let fast=head,slow=head;
-    // 让快指针比慢指针先走N步
+    // 让快指针比慢指针先走N步,这样快指针到终点，慢指针就在倒数第N个节点
     while(n-->0){
         fast=fast.next;
     }
