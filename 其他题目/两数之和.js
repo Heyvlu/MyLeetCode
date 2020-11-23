d1de033a836231da2581ff0091477aeac5597066
Heyvlu <3059982570@qq.com>
2020-11-23 18:47:46 +0800

    HashMap

--- 其他题目/两数之和.js
+++ 其他题目/两数之和.js
@@ -1,15 +1,15 @@
-
-    var twoSum = function(nums,target) {
-        if(nums==[]){
-            return false;
+var twoSum = function(nums, target) {
+    let hashMap={};
+    for(let i=0;i<nums.length;i++){
+        let num=target-nums[i];
+        let index=hashMap[num];
+        if(index!==undefined){
+            return[index,i];
         }else{
-            for(let i=0;i < nums.length; i++){
-                for(let j=0;j < nums.length;j++){
-                    if( nums[i]+nums[j]===target&&i!==j){
-                        return([i,j]);
-                    }
-                }
-            } 
-        } 
-    };
-    console.log(twoSum([3,3],6));
+            hashMap[nums[i]]=i;
+        }
+    }
+};
+// 用 hashMap 存储遍历过的元素和对应的索引。
+// 每遍历一个元素，看看 hashMap 中是否存在满足要求的目标数字。
+// 所有事情在一次遍历中完成（用了空间换取时间）。
