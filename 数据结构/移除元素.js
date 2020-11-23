53f934dcd1ff615b9eb7a2bfa9814e5a9d9024ca
Heyvlu <3059982570@qq.com>
2020-11-23 11:58:21 +0800

    Fast and slow pointer

---
+++ 数据结构/移除元素.js
@@ -0,0 +1,11 @@
+var removeElement = function(nums, val) {
+    let slow=0,fast=0;
+    while(fast<nums.length){
+        if(nums[fast]!=val){
+            nums[slow]=nums[fast];
+            slow++;
+        }
+        fast++;
+    }
+    return slow;
+};
