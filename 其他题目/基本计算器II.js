e811e4aba3bd638e16169080fc5eecf9d9dd27f2
Heyvlu <3059982570@qq.com>
2020-12-04 20:15:28 +0800

    recursion

---
+++ 其他题目/基本计算器II.js
@@ -0,0 +1,42 @@
+var calculate = function(s) {
+    let arr=Array.from(s).reverse();
+    let helper=(arr)=>{
+        let stack=[];
+        sign="+";
+        num=0;
+        while(arr.length>0){
+            let c=arr.pop();
+            if(/\d+/.test(c)){
+                num=num*10+(c-'0');
+            }
+            if(c=='('){
+                num=helper(arr);
+            }
+            if((!/\d+/.test(c)&&c!==' ')||arr.length==0){
+                if(sign=='+'){
+                    stack.push(num);
+                }else if(sign=='-'){
+                    stack.push(-num);
+                }else if(sign=='*'){
+                    pre=stack.pop();
+                    stack.push(pre*num);
+                }else if(sign=='/'){
+                    pre=stack.pop();
+                    stack.push(parseInt(pre/num));
+                }
+                sign=c;
+                num=0;
+            }
+            if(c==')'){
+                break;
+            }
+        }
+        let res=0;
+        while(stack.length!==0){
+            res+=stack.pop();
+        }
+        return res;
+    };
+    return helper(arr);
+};
+console.log(calculate(" 3/2"));
